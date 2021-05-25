6c20a79dee1b45a2821a7004f58bd161a0616659
Poucheong Chong <pochong@ucsc.edu>
2021-05-25 00:46:37 -0700

    added in url for cors

--- routes/cors.js
+++ routes/cors.js
@@ -2,7 +2,7 @@ const express = require('express')
 const cors = require('cors')
 const app = express();
 
-const whitelist = ['http://localhost:3000', 'http://localhost:3001']
+const whitelist = ['http://localhost:3000', 'http://localhost:3001', 'https://llibrary-novel.herokuapp.com/']
 var corsOptionsDelegate = (req, callback) => {
     var corsOptions;
     //console.log(whitelist.indexOf(req.header('Origin')));
